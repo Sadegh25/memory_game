e3c3a80bb0f20f0131f0631b9a6801e934188be6
sadegh <sadegh25_safikhani@yahoo.com>
2019-08-07 15:49:11 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTow = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log("up and running");
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = [ ];
 
-console.log("User flipped " + cardOne);
+console.log("User flipped" + cards[0]);
+    cardsInPlay.push(cards[0]);
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = [ ];
+    
+console.log("User flipped" + cards[2]);
+    cardsInPlay.push(cards[2]);
 
-console.log("User flipped " + cardThree);
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match");
+}   else {
+    console.log("sorry, try again.");
+} 
+}
+
+checkForMatch();
