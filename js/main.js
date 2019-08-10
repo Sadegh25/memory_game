91b12d3f9a3eed3ca1a53c15c342125a7ac12cae
sadegh <sadegh25_safikhani@yahoo.com>
2019-08-10 22:52:14 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,27 +1,48 @@
-console.log("up and running");
-var cards = ["queen", "queen", "king", "king"]
-var cardsInPlay = [ ];
+
+var cards = [
+    {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
+var cardsInPlay = []; 
 
 function checkForMatch() {
+     
     if (cardsInPlay[0] === cardsInPlay[1]) {
         console.log("You found a match");
     } else {
         console.log("sorry, try again.");
-    }
-    
-}
+    } 
+};
 
 
 function flipCard(cardId) {
     
-console.log("User flipped" + cards[cardId]);
-cardsInPlay.push(cards[0]);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].suit);
+console.log(cards[cardId].cardImage);
+cardsInPlay.push(cards[cardId].rank);
+if (cardsInPlay.length === 2) {
 
-if (cards.length === 2) {
 checkForMatch();
-} 
-
-}
+};
+};
 flipCard(0);
 flipCard(2);
-checkForMatch();
