780bec865b03e7edd918f7e8cc2419afde8dc1c0
sadegh <sadegh25_safikhani@yahoo.com>
2019-08-11 20:23:14 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ var cards = [
 var cardsInPlay = []; 
 
 function checkForMatch() {
-     
+    
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match");
     } else {
@@ -33,16 +33,25 @@ function checkForMatch() {
 };
 
 
-function flipCard(cardId) {
-    
+function flipCard() {
+    var cardId = this.getAttribute("data-id"); 
 console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].suit);
 console.log(cards[cardId].cardImage);
 cardsInPlay.push(cards[cardId].rank);
+this.setAttribute("src", cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 
 checkForMatch();
 };
 };
-flipCard(0);
-flipCard(2);
+function createBoard() {
+for (i=0; i<cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+ cardElement.setAttribute("data-id", i);
+ cardElement.addEventListener("click", flipCard);
+ document.getElementById("game-board").appendChild(cardElement);
+};
+};
+createBoard();
